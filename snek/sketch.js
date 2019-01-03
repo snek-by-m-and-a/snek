9581961d00db2d9f907aca3f736fc1ce7dcb89d1
Matt-the-Marxist <m.morrissette105000@gmail.com>
2019-01-03 13:05:24 -0500

    1/3/19 class

--- snek/sketch.js
+++ snek/sketch.js
@@ -1,7 +1,30 @@
+let snek;
+let scale = 10;
+
 function setup() {
-	
+	colorMode(HSB);
+	createCanvas(400,400);
+	snek = new Snake
+	frameRate(scale);
+}
+function keyPressed(){
+	if(keyCode == UP_ARROW && snek.yspeed != scale){
+		snek.direction(0,-1)
+	}
+	else if(keyCode == DOWN_ARROW && snek.yspeed != -scale){
+		snek.direction(0,1)
+	}
+	else if(keyCode == LEFT_ARROW && snek.xspeed != scale){
+		snek.direction(-1,0)
+	}
+	else if(keyCode == RIGHT_ARROW && snek.xspeed != -scale){
+		snek.direction(1,0)
+	}
 }
 
 function draw() {
+	background(0);
+	snek.update();
+	snek.show();
 	
 }
--- snek/snek.js
+++ snek/snek.js
@@ -1,3 +1,21 @@
-function snek(){
+function Snake(){
+	this.x = width/2;
+	this.y = height/2;
+	this.xspeed = scale;
+	this.yspeed = 0;
 	
+	this.update = function(){
+		this.x += this.xspeed;
+		this.y += this.yspeed;
+		this.x = constrain(this.x, 0, width-scale);
+		this.y = constrain(this.y, 0, height-scale);
+	}
+	this.show = function(){
+		fill(map(this.x,0,255),map(this.y,0,255),0);
+		rect(this.x, this.y, scale, scale);
+	}
+	this.direction = function(x,y){
+		this.xspeed = x*scale;
+		this.yspeed = y*scale;
+	}
 }
